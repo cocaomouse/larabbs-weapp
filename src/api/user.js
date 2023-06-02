216ba772b72b25983890b9c3846031b016e992ca
com <241288970@qq.com>
2023-06-03 00:33:10 +0800

    修改个人信息

--- src/api/user.js
+++ src/api/user.js
@@ -3,3 +3,11 @@ import { request, authRequest } from '@/utils/request'
 export function getCurrentUser(data) {
 	return authRequest('user')
 }
+
+/* 更新用户信息 */
+export function updateUser(data) {
+  return authRequest('user', {
+    method: 'put', // put方法更新用户信息，小程序不支持patch方法
+    data: data
+  })
+}
