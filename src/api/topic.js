cd5039c41807454c7e6d7e6604b9b86ec3807e44
com <241288970@qq.com>
2023-06-12 20:18:51 +0800

    话题详情页

--- src/api/topic.js
+++ src/api/topic.js
@@ -1,5 +1,6 @@
 import { request } from "@/utils/request";
 
+// 话题列表页
 export function getTopics(data) {
   return request('topics', {
     data: data
@@ -8,4 +9,11 @@ export function getTopics(data) {
 
 export function getCategories(data) {
   return request('categories')
+}
+
+// 话题详情
+export function getTopic(id, data) {
+  return request('topics/' + id, {
+    data: data
+  })
 }
