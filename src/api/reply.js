e1ddcaf5234dcb2921d4b86e111e1f9149a9d5c1
com <241288970@qq.com>
2023-06-17 19:42:55 +0800

    某个用户的回复列表

--- src/api/reply.js
+++ src/api/reply.js
@@ -1,7 +1,15 @@
 import { request, authRequest } from '@/utils/request'
 
+/* 话题回复列表 */
 export function getReplies(topicId, data) {
   return request('topics/' + topicId + '/replies',{
+    data: data
+  })
+}
+
+/* 用户的回复列表 */
+export function getUserReplies(userId, data) {
+  return request('users/' + userId + '/replies', {
     data: data
   })
 }
